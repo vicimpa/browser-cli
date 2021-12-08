6c6b0954d98f9ce654c01993bb6319434ce34f7a
PromiSe#### <vicimpa@ya.ru>
2021-12-08 03:44:59 +0300

    rename program

--- src/commands/counter.ts
+++ src/commands/counter.ts
@@ -4,11 +4,11 @@ export default async function (
   this: IContext,
   ...args: string[]
 ) {
-
+  this.log('Seconds counter.');
   let i = 0;
 
   while (true) {
-    this.log('Hi', i++);
+    this.log('Count: ', ++i);
     await delay(1000);
   }
 }
