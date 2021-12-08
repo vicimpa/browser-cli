12281cd16b7765af5efd8b22e06d0f046eb669a2
PromiSe#### <vicimpa@ya.ru>
2021-12-08 21:11:20 +0300

    Add uptime

---
+++ src/commands/uptime.ts
@@ -0,0 +1,14 @@
+import { getEnv } from "./lib/getEnv";
+
+export default () => {
+  const { startTime } = getEnv();
+  let delta = Date.now() - startTime;
+
+  let ms = delta % 1000;
+  let s = (delta /= 1000) | 0 % 60;
+  let m = (delta /= 60) | 0 % 60;
+  let h = (delta /= 60) | 0 % 24;
+  let d = (delta /= 24) | 0;
+
+  return `System uptime: ${d}d ${h}h ${m}m ${s}s ${m}ms`;
+};
